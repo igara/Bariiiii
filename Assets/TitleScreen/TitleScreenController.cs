18a70241563a456308f7904e0c4d80e9a6213948
五十嵐翔 <igara1119@gmail.com>
2016-05-21 23:52:31 +0900

    プロジェクトの追加

---
+++ Assets/TitleScreen/TitleScreenController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour {
+
+	/**
+	 * インスタンス生成された時のみ実行されるメソッド
+	 */
+	void Awake () {
+
+	}
+
+	/**
+	 * Awakeの後で
+	 * 最初のフレームのアップデート前に実行されるメソッド
+	 */
+	void Start () {
+	}
+	
+	/**
+	 * フレーム毎に一度実行されるメソッド
+	 */
+	void Update () {
+	}
+
+	/**
+	 * GUIイベントに応じて、フレームごとに複数回呼び出されるメソッド
+	 */
+	void OnGUI () {
+	}
+
+	/**
+	 * Scanボタンを押下した時の処理
+	 */
+	public void OnClickBarcordScanButton () {
+		SceneManager.LoadScene("BarcodeScanScreen/BarcodeScanScreenView");
+	}
+}
