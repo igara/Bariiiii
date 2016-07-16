b2c03ce3f8385f5c2fbdfa118688ec108557623f
五十嵐翔 <igara1119@gmail.com>
2016-07-16 18:42:25 +0900

    NugetをAssetsしたで管理させない

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 svn checkout https://zxingnet.svn.codeplex.com/svn/trunk/Clients/UnityDemo/Assets/ Assets/Plugins/ZXing
-dotnet restore --packages ./Assets/Plugins/Nuget
+dotnet restore --packages ./Nuget
