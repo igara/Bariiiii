fd40c0561c98ac8055f2f85ae48d1cad69c6034f
五十嵐翔 <igara1119@gmail.com>
2016-07-25 23:21:12 +0900

    入力するフィールドの

--- Assets/Screen/GoogleLoginScreen/GoogleLoginScreenCanvas.cs
+++ Assets/Screen/GoogleLoginScreen/GoogleLoginScreenCanvas.cs
@@ -4,15 +4,52 @@ using UnityEngine.SceneManagement;
 
 public class GoogleLoginScreenCanvas : MonoBehaviour {
 
-	// Use this for initialization
+	private ScreenAutorotateSetting screenAutorotateSetting = new ScreenAutorotateSetting();
+
+	/**
+	 * インスタンス生成された時のみ実行されるメソッド
+	 */
+	void Awake () {
+
+	}
+
+	/**
+	 * Awakeの後で
+	 * 最初のフレームのアップデート前に実行されるメソッド
+	 */
 	void Start () {
-	
 	}
-	
-	// Update is called once per frame
+
+	/**
+	 * フレーム毎に一度実行されるメソッド
+	 */
 	void Update () {
-	
 	}
+
+	/**
+	 * GUIイベントに応じて、フレームごとに複数回呼び出されるメソッド
+	 */
+	void OnGUI () {
+	}
+
+	/**
+	 * Behaviour が有効/アクティブになったときに呼び出される 
+	 */
+	void OnEnable() {
+		// 画面の回転を許可しない
+		screenAutorotateSetting.setAutorotateSwichFalse();
+		// immersiveモードを解除
+		Screen.fullScreen = false;
+	}
+
+	/**
+	 * Behaviour が無効/非アクティブになったときに呼び出される 
+	 */
+	void OnDisable() {
+		// 画面の回転を許可する
+		screenAutorotateSetting.setAutorotateSwichTrue();
+	}
+
 
 	public void OnClickBackButton () {
 		SceneManager.LoadScene ("TitleScreenView");
