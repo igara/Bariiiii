5efabb838d8966c4ff535d8e875d3ae761adc2b0
五十嵐翔 <igara1119@gmail.com>
2016-09-13 23:12:56 +0900

    プロジェクト名の変更

--- CreateBariiiiiAndroidNativePlugin/app/src/main/java/work/syonet/bariiiii/BarcodeCameraFragment.kt
+++ CreateBariiiiiAndroidNativePlugin/app/src/main/java/work/syonet/bariiiii/BarcodeCameraFragment.kt
@@ -1,4 +1,4 @@
-package work.syonet.createbariiiiiandroidnativeplugin.createbariiiiiandroidnativeplugin
+package work.syonet.bariiiii
 
 import android.content.Context
 import android.os.Bundle
--- CreateBariiiiiAndroidNativePlugin/app/src/main/java/work/syonet/bariiiii/BarcodeScanAndroidActivity.kt
+++ CreateBariiiiiAndroidNativePlugin/app/src/main/java/work/syonet/bariiiii/BarcodeScanAndroidActivity.kt
@@ -1,4 +1,4 @@
-package work.syonet.createbariiiiiandroidnativeplugin.createbariiiiiandroidnativeplugin
+package work.syonet.bariiiii
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
